c6762b7c0b25b8e4c4738027dc27494f16739840
recondep <164689950+recondep@users.noreply.github.com>
2025-09-16 10:06:03 -0500

    Add files via upload

--- 3. Archivo conexion/conexion.py
+++ 3. Archivo conexion/conexion.py
@@ -4,7 +4,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host='localhost',
         user='root',
-        password='tu_password',  # Cambia por tu contraseña
+        password='tu_password',
         database='desarrollo_web'
     )
     return connection
