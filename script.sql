1dc1ae0919fd78a558f5f8f66d92d5f57425b39b
recondep <164689950+recondep@users.noreply.github.com>
2025-09-16 10:41:26 -0500

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,5 @@
+git init
+git add .
+git commit -m "Implementación sistema login con Flask y MySQL"
+git remote add origin https://github.com/tu_usuario/tu_repositorio.git
+git push -u origin master
